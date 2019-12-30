ea37bf2fd271192fba97a6145e90f6dd4041de9c
User <dr.gordod>
2019-12-30 17:17:01 +0300

    v.2.0. Массив String "names" был переопределен в массив String "newNames" в котором появилось 4 место для String "Артур".  Был добавлен case "Артур" в switch.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,14 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	String[] names = {"Алексей","Рома","Данил"};
-        for (int i = 0; i < 4 ; i++) {
+	String[] newNames = Arrays.copyOf(names, names.length + 1);
+	newNames[names.length] = "Артур";
+        for (int i = 0; i < names.length ; i++) {
 	switch (names[i]) {
         case "Алексей":
             System.out.println("Доброе утро Алексей.");
@@ -14,6 +18,8 @@ public class Main {
             break;
         case "Данил":
             System.out.println("Добрый вечер Данил.");
+        case "Артур":
+            System.out.println("Доброй ночи Артур");
             break;
 	}
     }
